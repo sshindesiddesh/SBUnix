52ced85e323294dbc9e755c56f6bf745bf43750f
Siddesh Shinde <sshindesiddesh@gmail.com>
2017-09-01 11:28:35 -0400

    Generic support for all the commands supported by execcve/execvpe .
    Signed-off-by: Siddesh Shinde <sshindesiddesh@gmail.com>

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -74,16 +74,11 @@ void exec_cmd(const char *buf, char *const argv[], char *const envp[])
 
 	if (!str_cmp("exit", buf))
 		exit(EXIT_SUCCESS);
-	else if (!str_cmp("ls", buf))
-		goto EXEC;
-	else if (!str_cmp("clear", buf))
-		goto EXEC;
 	else if (!str_cmp("cd", buf)) {
 		if (!chdir(argv[1]))
 			return;
 	} else
-		return;
-EXEC:
+		;
 
 	fork();
 
