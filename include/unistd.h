e6bb1bc39947dc0f4cf240a327f461a2bea4472c
Siddesh Shinde <sshindesiddesh@gmail.com>
2017-09-04 17:29:26 -0400

    Add system calls open, close and fork
    Signed-off-by: Siddesh Shinde <sshindesiddesh@gmail.com>

--- include/unistd.h
+++ include/unistd.h
@@ -5,15 +5,16 @@
 
 size_t write(int fd, const void *buf, size_t count);
 size_t read(int fd, void *buf, size_t count);
-#if 0
 int open(const char *pathname, int flags);
 int close(int fd);
+pid_t fork();
+
+#if 0
 int unlink(const char *pathname);
 
 int chdir(const char *path);
 char *getcwd(char *buf, size_t size);
 
-pid_t fork();
 int execvpe(const char *file, char *const argv[], char *const envp[]);
 pid_t wait(int *status)
 int waitpid(int pid, int *status);
---
+++ libc/syscall/open_close.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int open(const char *pathname, int flags)
+{
+	size_t out;
+	__asm__ (
+		/* System Call Number */
+		"movq $2, %%rax\n"
+		/* Param 1 */
+		"movq %1, %%rdi\n"
+		/* Param 2 */
+		"movq %2, %%rsi\n"
+#if 0
+		/* Param 3 */
+		"movq %3, %%rdx\n"
+		/* Param 4 */
+		"movq %1, %%rcx\n"
+		/* Param 5 */
+		"movq %1, %%r8\n"
+		/* Param 6 */
+		"movq %1, %%r9\n"
+#endif
+		"syscall\n"
+		/* Output of the system call */
+		"movq %%rax, %0\n"
+		: "=m"(out)/* output parameters, we aren't outputting anything, no none */
+		/* (none) */
+		: /* input parameters mapped to %0 and %1, repsectively */
+		"m" (pathname), "m" (flags)
+		: /* registers that we are "clobbering", unneeded since we are calling exit */
+		"rax", "rdi", "rsi"
+	);
+	return out;
+}
+
+int close(int fd)
+{
+	size_t out;
+	__asm__ (
+		/* System Call Number */
+		"movq $3, %%rax\n"
+		/* Param 1 */
+		"movq %1, %%rdi\n"
+#if 0
+		/* Param 2 */
+		"movq %2, %%rsi\n"
+		/* Param 3 */
+		"movq %3, %%rdx\n"
+		/* Param 4 */
+		"movq %1, %%rcx\n"
+		/* Param 5 */
+		"movq %1, %%r8\n"
+		/* Param 6 */
+		"movq %1, %%r9\n"
+#endif
+		"syscall\n"
+		/* Output of the system call */
+		"movq %%rax, %0\n"
+		: "=m"(out)/* output parameters, we aren't outputting anything, no none */
+		/* (none) */
+		: /* input parameters mapped to %0 and %1, repsectively */
+		"m" (fd)
+		: /* registers that we are "clobbering", unneeded since we are calling exit */
+		"rax", "rdi"
+	);
+	return out;
+
+}
