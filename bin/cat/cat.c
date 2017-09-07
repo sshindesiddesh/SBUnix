b6d6d72101ca2eb0e97a3465b8d1660026a75391
Siddesh Shinde <sshindesiddesh@gmail.com>
2017-09-07 03:04:58 -0400

    Update cat.c with updated getc/putc calls.
    Signed-off-by: Siddesh Shinde <sshindesiddesh@gmail.com>

--- bin/cat/cat.c
+++ bin/cat/cat.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 #include <unistd.h>
 
+size_t get_line(int fp, char *buf)
+{
+	int ret = -1;
+	if ((ret = read(fp, buf, 1024)) == -1) {
+		exit(0);
+	}
+	buf[ret - 1] = '\0';
+	return ret;
+
+}
+
 int main(int argc, char *argv[])
 {
-	int x = 0;
+	int x = 1, c = 0;
 	if (argc == 2) {
-		FILE f = open(argv[1], O_RDWR);
+		int f = open(argv[1], O_RDONLY);
 		if (!f)
 			return 0;
-		while (x != EOF) {
-			x = getc(f);
-			putc(x, stdout);
+		while (x != 0) {
+			x = read(f, &c, 1);
+			write(1, &c, x);
 		}
 	}
         return 0;
