87e2a9bdae59ad8b841a36de1462623a01f61284
Siddesh Shinde <sshindesiddesh@gmail.com>
2017-10-28 01:43:25 -0400

    Temporary Changes
    Signed-off-by: Siddesh Shinde <sshindesiddesh@gmail.com>

--- sys/main.c
+++ sys/main.c
@@ -15,24 +15,17 @@ uint32_t* loader_stack;
 extern char kernmem, physbase;
 void clear();
 
-void ahci_init();
+//void ahci_init();
+void memory_init(uint32_t *modulep, void *physbase, void *physfree);
 
 void start(uint32_t *modulep, void *physbase, void *physfree)
 {
+	init_idt();
+	timer_init();
+	pic_init();
 	clear();
-	struct smap_t {
-		uint64_t base, length;
-		uint32_t type;
-	}__attribute__((packed)) *smap;
-	while(modulep[0] != 0x9001) modulep += modulep[1]+2;
-	for(smap = (struct smap_t*)(modulep+2); smap < (struct smap_t*)((char*)modulep+modulep[1]+2*4); ++smap) {
-		if (smap->type == 1 /* memory */ && smap->length != 0) {
-			kprintf("Available Physical Memory [%p-%p]\n", smap->base, smap->base + smap->length);
-		}
-	}
-	kprintf("physfree %p\n", (uint64_t)physfree);
-	kprintf("tarfs in [%p:%p]\n", &_binary_tarfs_start, &_binary_tarfs_end);
-	ahci_init();
+	memory_init(modulep, physbase, physfree);
+	//ahci_init();
 }
 
 void boot(void)
@@ -49,10 +42,6 @@ void boot(void)
     :"r"(&initial_stack[INITIAL_STACK_SIZE])
   );
   init_gdt();
-  init_idt();
-  timer_init();
-  pic_init();
-
   start(
     (uint32_t*)((char*)(uint64_t)loader_stack[3] + (uint64_t)&kernmem - (uint64_t)&physbase),
     (uint64_t*)&physbase,
--- sys/memory.c
+++ sys/memory.c
@@ -7,25 +7,34 @@
 
 extern char kernmem, physbase;
 
-struct smap_t {
-	uint64_t base, length;
-	uint32_t type;
-}__attribute__((packed)) *smap;
-
 typedef struct page_dir {
 	struct page_dir *next;
 	int acc;
 } page_dir_t;
 
-static uint64_t *phys_free;
-static int cnt = 0;
+static uint64_t phys_free;
 static page_dir_t *free_list_ptr = 0;
 static page_dir_t *pd_prev = 0;
+static uint64_t total_pages = 0;
 
-void create_page_dir(uint64_t start, uint64_t length, void *physbase)
+page_dir_t *get_free_pages(int n)
+{
+	if (!free_list_ptr)
+		return 0;
+	int i = n + 1;
+	page_dir_t *ptr = free_list_ptr;
+	while (i--) {
+		free_list_ptr->acc = 0;
+		free_list_ptr = free_list_ptr->next;
+	}
+	return ptr;
+}
+
+void create_page_disc(uint64_t start, uint64_t length, void *physbase)
 {
 	page_dir_t *pd_cur = (page_dir_t *)phys_free;
 	uint64_t no_page = length/PG_SIZE;
+	total_pages += no_page;
 
 	uint64_t i = start/PG_SIZE;
 	for (; i < no_page; i++) {
@@ -47,18 +56,28 @@ void create_page_dir(uint64_t start, uint64_t length, void *physbase)
 
 void memory_init(uint32_t *modulep, void *physbase, void *physfree)
 {
-	phys_free = physfree;
+
+	struct smap_t {
+		uint64_t base, length;
+		uint32_t type;
+	}__attribute__((packed)) *smap;
+
+	phys_free = (uint64_t)physfree;
 	while(modulep[0] != 0x9001) modulep += modulep[1]+2;
 	for(smap = (struct smap_t*)(modulep+2); smap < (struct smap_t*)((char*)modulep+modulep[1]+2*4); ++smap) {
 		if (smap->type == 1 /* memory */ && smap->length != 0) {
 			kprintf("Available Physical Memory [%p-%p] Length %p\n", smap->base, smap->base + smap->length, smap->length);
 			if (smap->base == 0) {
 				//kprintf("Available Pages %d\n", smap->length/PG_SIZE);
-				create_page_dir(smap->base, smap->length, physbase);
+				create_page_disc(smap->base, smap->length, physbase);
+				kprintf("TP %d\n", total_pages);
 			} else {
 				//kprintf("Available Pages %d\n", (smap->base+smap->length-(uint64_t)physfree)/PG_SIZE);
 				//no_page = (smap->base+smap->length-(uint64_t)physfree)/PG_SIZE;
-				create_page_dir(smap->base, smap->length, physbase);
+				create_page_disc(smap->base, smap->length, physbase);
+				kprintf("TP %d\n", total_pages);
+				/* TODO: Remove this */
+				break;
 			}
 		}
 	}
@@ -70,4 +89,7 @@ void memory_init(uint32_t *modulep, void *physbase, void *physfree)
 		kprintf("%p \n", ptr->next);
 		ptr = ptr->next;
 	}
+	kprintf("TP %d\n", total_pages);
+	get_free_pages((total_pages*sizeof(page_dir_t))/PG_SIZE);
+	kprintf("%p \n", free_list_ptr);
 }
