f0792e8b68f62e0b53a237de180caa59bbef9b11
Siddesh Shinde <sshindesiddesh@gmail.com>
2017-10-29 02:37:46 -0400

    [Clean-up] Memory Subsystem
    Signed-off-by: Siddesh Shinde <sshindesiddesh@gmail.com>

--- sys/memory.c
+++ sys/memory.c
@@ -34,7 +34,9 @@ typedef struct page_dir {
 	int acc;
 } page_dir_t;
 
-uint64_t phys_base;
+
+static uint64_t *pml;
+static uint64_t phys_base;
 static uint64_t phys_free;
 static uint64_t phys_end;
 static page_dir_t *free_list_ptr = 0;
@@ -50,23 +52,14 @@ uint64_t *get_free_pages(uint64_t n)
 		return 0;
 	int i = n;
 	page_dir_t *ptr = free_list_ptr;
-	#ifdef K_DEBUG
-	kprintf("fp %p %p n %p n ", free_list_ptr, (uint64_t)free_list_ptr - KERNBASE, free_list_ptr->next);
-	#endif
 	while (i--) {
 		free_list_ptr->acc = 0;
 		free_list_ptr = free_list_ptr->next;
 	}
 	ptr = (page_dir_t *)((((uint64_t)ptr - (uint64_t)phys_free - (uint64_t)KERNBASE)/sizeof(page_dir_t)*PG_SIZE));
-	#ifdef K_DEBUG
-	kprintf("ptr %p 2: %p", ptr, (uint64_t)ptr + KERNBASE);
-	#endif
 	
 	/* TODO: Remove ?? -> ZERO OUT PAGE */
 	memset((void *)((uint64_t)ptr + KERNBASE), 0, PG_SIZE);
-	kprintf(" NP %p ", ptr);
-#ifdef K_DEBUG
-#endif
 	return (uint64_t *)(uint64_t)ptr;
 }
 
@@ -88,7 +81,9 @@ void create_page_disc(uint64_t start, uint64_t length, void *physbase)
 		} else {
 			if (free_list_ptr == 0) {
 				free_list_ptr = (page_dir_t *)(pd_cur + i);
+#ifdef K_DEBUG
 				kprintf("FP %p \n", free_list_ptr);
+#endif
 			}
 			pd_cur[i].acc = 1;
 			pd_cur[i].next = 0;
@@ -102,21 +97,21 @@ void create_page_disc(uint64_t start, uint64_t length, void *physbase)
 uint64_t *get_pte_from_pgdir(uint64_t *pgdir, uint64_t va)
 {
 	uint64_t *pte_ptr, *pte;
-	#ifdef K_DEBUG
+#ifdef K_DEBUG
 	kprintf(" PDX %x ", PDX(va));
-	kprintf(" r pgdir %p ", pgdir);
-	#endif
+	kprintf(" pgdir %p ", pgdir);
+#endif
 	if (!(pgdir[PDX(va)] & PTE_P)) {
 		pte_ptr = get_free_pages(1);
-		#ifdef K_DEBUG
+#ifdef K_DEBUG
 		kprintf(" pte %p ", pte_ptr);
-		#endif
+#endif
 		pgdir[PDX(va)] = ((uint64_t)pte_ptr | PTE_P | PTE_U | PTE_W);
 	} else {
 		pte_ptr = (uint64_t *)(pgdir[PDX(va)] & ~(0xFFF));
-		#ifdef K_DEBUG
-		kprintf(" ISSUE PGDIR %p ", pte_ptr);
-		#endif
+#ifdef K_DEBUG
+		kprintf(" UA PGDIR %p ", pte_ptr);
+#endif
 	}
 
 	pte = &pte_ptr[PTX(va)];
@@ -126,20 +121,17 @@ uint64_t *get_pte_from_pgdir(uint64_t *pgdir, uint64_t va)
 uint64_t *get_pte_from_pdpe(uint64_t *pdpe, uint64_t va)
 {
 	uint64_t *pgdir, *pte;
-	#ifdef K_DEBUG
+#ifdef K_DEBUG
 	kprintf(" PDPE %p ", pdpe[PDPE(va)]);
-	#endif
+#endif
 	if (!(pdpe[PDPE(va)] & PTE_P)) {
 		pgdir = get_free_pages(1);
-		#ifdef K_DEBUG
-		#endif
 		pdpe[PDPE(va)] = ((uint64_t)pgdir | PTE_P | PTE_U | PTE_W);
-		kprintf(" 2 :PDPE %p ", pdpe[PDPE(va)]);
 	} else {
 		pgdir = (uint64_t *)(pdpe[PDPE(va)] & ~(0xFFF));
-		#ifdef K_DEBUG
+#ifdef K_DEBUG
 		kprintf(" ISSUE PDPE ");
-		#endif
+#endif
 	}
 
 	pte = get_pte_from_pgdir(pgdir, va);
@@ -150,22 +142,21 @@ uint64_t *get_pte_from_pml(uint64_t *pml, uint64_t va)
 {
 	uint64_t *pdpe, *pte;
 
-	#ifdef K_DEBUG
+#ifdef K_DEBUG
 	kprintf(" PML %x ", PML4(va));
-	#endif
+#endif
 	if (!(pml[PML4(va)] & PTE_P)) {
 		pdpe = get_free_pages(1);
-		#ifdef K_DEBUG
+#ifdef K_DEBUG
 		kprintf(" pdpe %p ", pdpe);
-		#endif
+#endif
 		pml[PML4(va)] = ((uint64_t)pdpe | PTE_P | PTE_U | PTE_W);
 	} else {
 		pdpe = (uint64_t *)(pml[PML4(va)] & ~(0xFFF));
-		#ifdef K_DEBUG
+#ifdef K_DEBUG
 		kprintf(" ISSUE PML %p ", pdpe);
-		#endif
+#endif
 	}
-
 	pte = get_pte_from_pdpe(pdpe, va);
 	return pte;
 }
@@ -177,22 +168,22 @@ void map_page_entry(uint64_t *pml, uint64_t va, uint64_t size, uint64_t pa, uint
 	for (i = 0; i < size; i += PG_SIZE) {
 		pte_ptr = get_pte_from_pml(pml, va + i);
 		if (pte_ptr) {
-		#ifdef K_DEBUG
+#ifdef K_DEBUG
 			kprintf(" v %p p %p ptr %p\n", va + i, pa + i, pte_ptr);
-		#endif
+#endif
 			*pte_ptr = pa + i;
 			*pte_ptr |= (PTE_P | PTE_U | PTE_W);
 		}
 	}
 }
-static uint64_t *pml;
 
 void page_table_init()
 {
 	pml = get_free_pages(1);
+#ifdef K_DEBUG
 	kprintf("pml %p ", pml);
+#endif
 	map_page_entry(pml, VA, (phys_end - phys_base), (uint64_t)phys_base, 0);
-	//map_page_entry(pml, VA, 8 * 4096, (uint64_t)phys_base, 0);
 	map_page_entry(pml, KERNBASE + 0xB8000, 4 * 4096, (uint64_t)0xB8000, 0);
 }
 
@@ -223,14 +214,6 @@ void memory_init(uint32_t *modulep, void *physbase, void *physfree)
 	kprintf("physbase %p\n", (uint64_t)physbase);
 	kprintf("physfree %p\n", (uint64_t)physfree);
 	kprintf("tarfs in [%p:%p]\n", &_binary_tarfs_start, &_binary_tarfs_end);
-#if 0
-	page_dir_t *ptr = free_list_ptr;
-	int i = 0;
-	for (i = 0; i < 10; i++) {
-		kprintf("%x \n", ptr->next);
-		ptr = ptr->next;
-	}
-#endif	
 	page_table_init();
 	__asm__ volatile("mov %0, %%cr3":: "b"(pml));
 	change_console_ptr();
