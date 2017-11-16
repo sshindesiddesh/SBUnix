b11a5ae2bacf23386faef6bb2a8e625f3794ebe4
dhanashripp <dhanashripp@gmail.com>
2017-11-15 23:28:23 -0500

    tarfs debugging code

--- sys/tarfs.c
+++ sys/tarfs.c
@@ -7,6 +7,7 @@
 #include <sys/console.h>
 #include <sys/idt.h>
 #include <sys/kutils.h>
+#define TARFS_DEBUG 1
 
 static tarfs_entry_t tarfs_fs[100];
 
@@ -28,7 +29,9 @@ int get_parent_index(char* dir, int type)
 	int i = 0;
 	while(strcmp(&name[0], &(tarfs_fs[i].name[0])) !=  0)
 		i++;
-	//kprintf("\tparent : %d", i);
+#ifdef TARFS_DEBUG
+	kprintf("\tparent : %d", i);
+#endif
 	return i;
 }	
 
@@ -41,11 +44,15 @@ uint64_t check_file_exists(char* filename)
 	{
 		tmp_tarfs = (struct posix_header_ustar *)(&_binary_tarfs_start + t);
 		size = octal_to_decimal(stoi(tmp_tarfs->size));
+#ifdef TARFS_DEBUG
 		kprintf("\tfile name:%s size:%d", tmp_tarfs->name, size);
+#endif
 		if (strlen(tmp_tarfs->name) == 0)
 			return 999;
 		if (!strcmp(tmp_tarfs->name, filename)) {
+#ifdef TARFS_DEBUG
 			kprintf("\t file found.!!");
+#endif
 			return t + 512;
 		}
 		if (size == 0)
@@ -62,7 +69,12 @@ int is_proper_executable(Elf64_Ehdr* header)
 		return -1;
 	else {
 		if (header->e_ident[1] == 'E' && header->e_ident[2] == 'L' && header->e_ident[3] == 'F')
+		{
+#ifdef TARFS_DEBUG
+			kprintf("\t executable verified");
+#endif	
 			return 0;
+		}
 	}
 	return -1;
 }
@@ -73,12 +85,17 @@ vma_t *malloc_vma(mm_t *mm)
         char  *tmp;
         tmp = (char *)kmalloc(sizeof(vma_t));
         if(mm->vma_list == NULL) {
+#ifdef TARFS_DEBUG
+		kprintf("\t first VMA");
+#endif
                 tmp = (char *)kmalloc(sizeof(vma_t));
                 vm_tail = (vma_t *)tmp;
                 mm->vma_list = vm_tail;
         }
         else {
+#ifdef TARFS_DEBUG
                 kprintf("\t not first VMA");
+#endif
                 vm_tail = mm->vma_list;
                 while (vm_tail->next != NULL)
                         vm_tail = vm_tail->next;
@@ -93,22 +110,29 @@ pcb_t * load_elf_code(pcb_t * pcb, Elf64_Ehdr * elf_header, char * filename)
 {
 	Elf64_Phdr* pgm_header;
 	mm_t *mms = pcb->mm;
+#ifdef TARFS_DEBUG
 	if (!mms)
 		kprintf("\tPCB MM empty");
+#endif
 	vma_t *new_node;
 	pgm_header = (Elf64_Phdr *) ((void *)elf_header + elf_header->e_phoff);
 	for (int i = 0 ; i < elf_header->e_phnum; ++i) {
 		if ((int)pgm_header->p_type == 1) { /* 1 is PT_LOAD, specifies a loadable segment */
 			if (pgm_header->p_filesz > pgm_header->p_memsz)
+#ifdef TARFS_DEBUG
 				kprintf("something wrong in Elf binary..");
+#endif
+				//memcpy((void*) pgm_header->p_vaddr, (void*) elf_header + pgm_header->p_offset, pgm_header->p_filesz);
 #if 0
 			else
 				memset((char*) pgm_header->p_vaddr + pgm_header->p_filesz, 0, pgm_header->p_memsz - pgm_header->p_filesz);
 			memcpy((char*) pgm_header->p_vaddr, (void *) elf_header + pgm_header->p_offset, pgm_header->p_filesz);
 #endif
 			new_node = malloc_vma(mms);
+#ifdef TARFS_DEBUG
 			if (!new_node)
 				kprintf("\t VMA not allocated");
+#endif
 			new_node->vma_start = pgm_header->p_vaddr;
 			new_node->vma_end = new_node->vma_start + pgm_header->p_memsz;
 			new_node->vma_flags = pgm_header->p_flags;
@@ -139,10 +163,14 @@ void tarfs_init()
 	while(1) {
 		tarfs_itr = (struct posix_header_ustar *)(&_binary_tarfs_start + t);
 		if(strlen(tarfs_itr->name) == 0) {
-			//kprintf("\tFile name empty");
+#ifdef TARFS_DEBUG
+			kprintf("\tFile name empty");
+#endif
 			break;
 		}
+#ifdef TARFS_DEBUG
 		kprintf("\t %s", tarfs_itr->name);
+#endif
 		strcpy(&new_entry->name[0], tarfs_itr->name);
 		size = 0;
 		/* detecting the size of file */
@@ -164,9 +192,10 @@ void tarfs_init()
 		else
 			t +=  (size % 512 == 0) ? size + 512 : size + (512 - size % 512) + 512;
 	}
+#ifdef TARFS_DEBUG
 	for (i = 0; i < 20; i++)
 		kprintf("\t new netry %s", tarfs_fs[i].name);
-	//kprintf("\nwhile complete");
+#endif
 }
 
 pcb_t * create_elf_process(char *filename, char *argv[])
@@ -176,16 +205,20 @@ pcb_t * create_elf_process(char *filename, char *argv[])
 		kprintf("\tFile not found");
 	else
 	{
+#ifdef TARFS_DEBUG
 		kprintf("\tFile found at offset : %d",offset);
+#endif
 		/* load binary */
 		Elf64_Ehdr* elf_header = (Elf64_Ehdr *)(&_binary_tarfs_start + offset);
 		if (is_proper_executable(elf_header) != -1) {
+#ifdef TARFS_DEBUG
 			kprintf("\tElf proper");
+#endif
 			pcb_t * user_proc = (pcb_t *)kmalloc(sizeof(pcb_t));
 			mm_t * new_mm = (mm_t *)kmalloc(sizeof(mm_t));
 			user_proc->mm = new_mm;
-			//return NULL;
-			return load_elf_code(user_proc, elf_header, filename);
+			//return load_elf_code(user_proc, elf_header, filename);
+			return NULL;
 		}
 	}
 	return NULL;
