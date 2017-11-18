7ee0146160e3b6eccdd04779eacdf814d41763c3
Siddesh Shinde <sshindesiddesh@gmail.com>
2017-11-17 21:22:54 -0500

    Add user malloc system call.
    Not completely tested. Very basic usage.
    
    Signed-off-by: Siddesh Shinde <sshindesiddesh@gmail.com>

--- sys/syscall.c
+++ sys/syscall.c
@@ -1,5 +1,6 @@
 #include <sys/kprintf.h>
 #include <sys/syscall.h>
+#include <sys/memory.h>
 
 void yield();
 
@@ -9,6 +10,8 @@ uint64_t __isr_syscall(syscall_in *in)
 		kprintf("%s", (char *)in->first_param);
 	} else if (in->syscall_no == 2) {
 		yield();
+	} else if (in->syscall_no == 3) {
+		kmalloc_user((uint64_t)in->first_param);
 	} else {
 		kprintf("SBUnix x86_64 OS\n");
 	}
