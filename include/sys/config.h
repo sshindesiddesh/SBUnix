ff81dd304cc89a5ac79b0a0d9fbab9add78b4aae
Siddesh Shinde <sshindesiddesh@gmail.com>
2017-11-19 00:41:12 -0500

    Bug-Fix Macro declared twice in sys/config.h
    Signed-off-by: Siddesh Shinde <sshindesiddesh@gmail.com>

--- include/sys/config.h
+++ include/sys/config.h
@@ -3,7 +3,6 @@
 
 #define ENABLE_TIMER		0
 #define ENABLE_KEYBOARD		0
-#define ENABLE_KEYBOARD		0
 /* User paging should be disabled for using user processes code in kernel space
  * e.g. simple functions. */
 #define ENABLE_USER_PAGING	1
