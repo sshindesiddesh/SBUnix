aab496cdd3b99f858f7ec3e9034ca943915e7982
Siddesh Shinde <sshindesiddesh@gmail.com>
2017-11-19 20:57:48 -0500

    update syscalls
    Signed-off-by: Siddesh Shinde <sshindesiddesh@gmail.com>

--- sys/syscall.c
+++ sys/syscall.c
@@ -4,17 +4,37 @@
 
 void yield();
 
+#define SYSCALL_READ	0
+#define SYSCALL_WRITE	1
+#define SYSCALL_OPEN	2
+#define SYSCALL_CLOSE	3
+#define SYSCALL_MMAP	9
+#define SYSCALL_INFO	99
+#define SYSCALL_YIELD	24
+
 uint64_t __isr_syscall(syscall_in *in)
 {
-	if (in->syscall_no == 1) {
-		kprintf("%s", (char *)in->first_param);
-	} else if (in->syscall_no == 2) {
-		yield();
-	} else if (in->syscall_no == 3) {
-		/* kmalloc signature has changed. */
-		/* kmalloc_user((uint64_t)in->first_param); */
-	} else {
-		kprintf("SBUnix x86_64 OS\n");
+	switch (in->syscall_no) {
+		case SYSCALL_READ:
+			break;
+		case SYSCALL_WRITE:
+			kprintf("%s", (char *)in->first_param);
+			break;
+		case SYSCALL_OPEN:
+			break;
+		case SYSCALL_CLOSE:
+			break;
+		case SYSCALL_MMAP:
+			break;
+		case SYSCALL_YIELD:
+			yield();
+			break;
+		case SYSCALL_INFO:
+			kprintf("SBUnix x86_64 OS\n");
+			break;
+		default:
+			kprintf("Error: Unknown System Call\n");
+			break;
 	}
 	return 0;
 }
--- sys/syscall.s
+++ sys/syscall.s
@@ -3,13 +3,13 @@
 
 .global __syscall_yield
 __syscall_yield:
-	mov $2, %rax
+	mov $24, %rax
 	int $0x80
 	retq
 
 .global __syscall_info
 __syscall_info:
-	mov $0, %rax
+	mov $99, %rax
 	int $0x80
 	retq
 
