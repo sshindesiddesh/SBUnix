49ce1e331f23c8025b0d132c87f7d15f285015dc
dhanashripp <dhanashripp@gmail.com>
2017-11-22 17:26:35 -0500

    CAT tested, temporary checkin

--- bin/cat/cat.c
+++ bin/cat/cat.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
-
+char* strcpy(char *dst, const char *src);
 
 int put_c(int c, int fd)
 {
@@ -26,6 +26,28 @@ size_t get_line(int fp, char *buf)
 
 int main(int argc, char *argv[])
 {
+	/* TODO: need to change the main function, temorary testing done */
+	char buf[1024] = "\0";
+	while (1) {
+		__asm__ volatile("movq $99, %rax");
+		__asm__ volatile("int $0x80");
+		int x = 1, c = 0;
+			int f = open("dpp/abc.txt", O_RDONLY, 444);
+			if (f < 0) {
+				strcpy(buf, "open failed");
+				write(0, buf, 10);
+				return 0;
+			}
+			while (x != 0) {
+				x = read(f, &c, 1);
+				write(0, &c, 1);
+			}
+		while(1);
+		__asm__ volatile("movq $24, %rax");
+		__asm__ volatile("int $0x80");
+	}	
+
+#if 0
 	int x = 1, c = 0;
 	if (argc == 2) {
 		int f = open(argv[1], O_RDONLY, 444);
@@ -39,4 +61,5 @@ int main(int argc, char *argv[])
 		}
 	}
         return 0;
+#endif
 }
