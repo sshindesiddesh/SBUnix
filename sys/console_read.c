d97afce5bf9d84224a8951565008e703923faf8c
dhanashripp <dhanashripp@gmail.com>
2017-11-23 14:09:13 -0500

    read stdin bug fix

--- sys/console_read.c
+++ sys/console_read.c
@@ -5,11 +5,11 @@
 
 /*TODO: Decide this size */
 #define CONSOLE_BUF_SIZE	1000
-
 char console_read_buf[CONSOLE_BUF_SIZE];
 
 uint64_t cursor = 0;
 volatile uint64_t reading_flag = 0;
+static volatile int scanlen = 0;
 
 #define ENTER '\n'
 
@@ -29,13 +29,25 @@ void update_read_buf(char key)
 uint64_t console_read(int fd, char *buf, uint64_t count)
 {
 	cursor = 0;
+	scanlen = 0;
 	reading_flag = 1;
+	__asm__ volatile ("sti");
 	while (reading_flag == 1);
-	__asm__ volatile ("cli");
 	buf[cursor] = '\0';
+
 	/* TODO: memcpy does not seem to work here.  */
 	strcpy(buf, console_read_buf);
 	cursor = CONSOLE_BUF_SIZE - 1;
-	__asm__ volatile ("sti");
+
+	/* clear console_read_buf */
+	while(console_read_buf[scanlen])
+		scanlen++;
+
+	while(scanlen >= 0) {
+		console_read_buf[scanlen] = '\0';
+		scanlen--;
+	}
+
+	__asm__ volatile ("cli");
 	return cursor;
 }
