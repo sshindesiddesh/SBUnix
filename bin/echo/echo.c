aecbbc840a74f3d53fcecde3283b1465e889b387
dhanashripp <dhanashripp@gmail.com>
2017-12-05 01:27:07 -0500

    echo with bug

--- bin/echo/echo.c
+++ bin/echo/echo.c
@@ -2,20 +2,31 @@
 #include <dirent.h>
 #include <unistd.h>
 
+//char *getenv(const char *name);
+//char* strcpy(char *dst, const char *src);
+
 int main(int argc, char *argv[])
 {
+	printf("in echo %d\n", argc);
 	if (argc < 2) {
 		puts("\n");
 	} else {
+		printf("%s\n", argv[1]);
 		if(*argv[1] == '$') {
+			printf("Here\n");
+			char *buf = getenv("PATH");
+			printf("%p\n", buf);
 			/* echo env var if available */
-			//char temp[50];
-		
-			
+			//char buf[100] = "\0";
+			//puts(getenv(argv[1] + 1));
+			//strcpy(buf, getenv(&(*argv[1] + 1)));
+			//puts(((char *)&(argv[1][1])));
+			//puts(getenv((char *)&(argv[1][1])));
+			//printf("%s\n", getenv("PATH"));
 		} else {
-			int i = 0;
-			while(argc >= 1) {
-				puts(argv[i+1]);
+			int i = 1;
+			while(argc > 1) {
+				puts(argv[i]);
 				puts(" ");
 				i++;
 				argc--;
