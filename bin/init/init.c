18ff7970b83154390ea1fccaeeb75d934c54b0ec
Siddesh Shinde <sshindesiddesh@gmail.com>
2017-12-07 08:23:27 -0500

    [init] Modify init process
    Signed-off-by: Siddesh Shinde <sshindesiddesh@gmail.com>

--- bin/init/init.c
+++ bin/init/init.c
@@ -3,19 +3,11 @@
 
 int main(int argc, char *argv[], char *env[])
 {
-	char * const argv_my[] = {"/rootfs/bin/echo", "===========================Welcome===========================\n", NULL};
+	char * const argv_my[] = {"/rootfs/bin/sbush", "/rootfs/etc/rc", NULL};
 	char * const env_my[] = {"PATH=/rootfs/bin", NULL};
 	pid_t pid = fork();
 	if (pid == 0) {
-		execvpe("/rootfs/bin/echo", argv_my, env_my);
-		exit(0);
-	}
-	waitpid(pid, 0);
-
-	char * const argv_my1[] = {"/rootfs/bin/sbush", "/rootfs/etc/rc", NULL};
-	pid = fork();
-	if (pid == 0) {
-		execvpe("/rootfs/bin/sbush", argv_my1, env_my);
+		execvpe("/rootfs/bin/sbush", argv_my, env_my);
 		exit(0);
 	}
 
@@ -30,6 +22,5 @@ int main(int argc, char *argv[], char *env[])
 
 	puts("\nSBUSH was killed.. Shutting down system...\n");
 	shutdown();
-		while (1);
 	return 0;
 }
