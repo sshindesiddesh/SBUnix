a901a980af1fc6b37c9ba59a1d4f7606a68588d1
dhanashripp <dhanashripp@gmail.com>
2017-12-07 05:10:48 -0500

    added testing related files

---
+++ bin/test3/test3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/defs.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+	int i = 0;
+	char *a, *b, *c;
+	while(i < 100000) {
+		a = (char *)malloc(900000);
+		strcpy(a, "hello there");
+		//puts(a);
+		b = (char *)malloc(1000);
+		free(a);
+		free(b);
+		a = (char *)malloc(1000);
+		c = (char *)malloc(200);
+		strcpy(c, "hello there c");
+		//puts(c);
+		free(c);
+		free(a);
+		i++;
+	}
+	write(1, "\nmalloc tested", 20);
+	return 0;
+}
