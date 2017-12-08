9c1c5a2ed43112fd607ce7eae4aa2c8461369b78
dhanashripp <dhanashripp@gmail.com>
2017-12-08 07:29:15 -0500

    malloc_free and opendir, readdir, closedir, open, close test files

---
+++ bin/open_close/open_close.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+/* binary to test all basic functionality at a single point */
+char buffer[100] = "\0";
+
+int main(int argc, char *argv[], char *env[])
+{
+	/* test for open, read, close in a file, assuming /rootfs/bin/cat already present */
+	int f, x = 1, c = 0;
+	f = open("/rootfs/bin/cat", O_RDONLY, 444);
+
+	if (f < 0) {
+		write(1, "\nCould not open the file", 30);
+	} else {
+		printf("content of cat binary:\n");
+		while (x != 0) {
+			x = read(f, &c, 1);
+			write(1, &c, 1);
+		}
+		close(f);
+	}
+
+	/* Opendir Readdir Closedir */
+	DIR * dir = opendir("/rootfs/etc1/");
+	if(dir == NULL) {
+		puts("\nDirectory does not exist");
+	} else {
+		puts("\nDirectory exists");
+	}
+
+	dir = opendir("/rootfs/bin/");
+	if(dir == NULL) {
+		puts("\nDirectory does not exist");
+	} else {
+		puts("\nDirectory exists, content:\n");
+
+		struct dirent* cur_dir = NULL;
+		while((cur_dir = readdir(dir)) != NULL) {
+			puts(cur_dir->d_name);
+			puts("    ");
+		}
+		closedir(dir);
+
+	}
+
+	return 0;
+}
