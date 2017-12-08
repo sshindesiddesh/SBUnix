3a9c6be95ac55f65c679327deb9c3ff14b4b5a2d
Siddesh Shinde <sshindesiddesh@gmail.com>
2017-12-08 15:53:12 -0500

    Remove test case file
    Signed-off-by: Siddesh Shinde <sshindesiddesh@gmail.com>

--- bin/mul_fork/mul_fork.c
+++ bin/mul_fork/mul_fork.c
@@ -5,12 +5,13 @@ int main(int argc, char *argv[], char *envp[])
 {
 	int a = 0;
 	pid_t pid;
+	char *argv1[] = {"/rootfs/bin/echo", "Hello\n", NULL};
 
-	while (a < 100) {
-	pid = fork();
-	if (pid == 0) {
-		execvpe("/rootfs/bin/test10", NULL, NULL);
-	}
+	while (a < 20) {
+		pid = fork();
+		if (pid == 0) {
+			execvpe("/rootfs/bin/echo", argv1, NULL);
+		}
 		a++;
 	}
 	puts("Parrent Spawned...\n");
